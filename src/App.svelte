83a6b73dbede3a7e7120b6cdd9cc1496d511ab92
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2023-08-12 22:42:16 +0700

    Add Footprints and Update main page

--- src/App.svelte
+++ src/App.svelte
@@ -15,14 +15,15 @@
   <h1 class="text-red-600 font-bold text-6xl">
     Pheu Thai Digital Footprint
   </h1>
+  <a href={Footprint[entry].link} target="_blank">
+    <h1 class="text-3xl font-bold text-white mt-6">
+      "{Footprint[entry].quote}" 
+      <span class="italic font-normal text-xl">
+        - {Footprint[entry].person}
+      </span>
+    </h1>
+  </a>
 
-  <h1 class="text-3xl font-bold text-white mt-6">
-    "{Footprint[entry].quote}" 
-    <span class="italic font-normal text-xl">
-      - {Footprint[entry].person}
-    </span>
-  </h1>
-{entry}
   <button class="mt-10 p-4 rounded text-blue-700 bg-white" on:click={newFootprint}>
     หาดิจิตอลฟุตพริ้นท์อันใหม่
   </button>
